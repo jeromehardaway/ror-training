7c6b5ce1b5a31d2d4cc124a3e3ce89de3d2ec8a0
Jerome Hardaway <jerome@vetswhocode.io>
2017-08-25 01:15:53 -0500

    completed up to ex5 on lrthw

---
+++ lrthw/ex5.rb
@@ -0,0 +1,17 @@
+my_name = "Jerome Hardaway"
+my_age = 31
+my_height = 68 # inches
+my_weight = 230 # lbs
+my_eyes = "Brown"
+my_teeth = "Black"
+my_hair = "Brown"
+
+puts "Let's talk about #{my_name}"
+puts "I'm #{my_height} inches tall."
+puts "I'm #{my_weight} pounds heavy."
+puts " Dang man, you've gained a lot of weight."
+puts  " I have #{my_eyes} eyes and #{my_hair} hair."
+puts " I teeth are usually #{my_teeth} depending on the coffee or matcha."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight}
+I get #{my_age + my_height + my_weight}."

3a62752cdd58febbf697e98e522278dc35f0b085
Jerome Hardaway <jerome@vetswhocode.io>
2017-08-28 12:33:42 -0500

    save

---
+++ lrthw/ex34.rb
@@ -0,0 +1,11 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is #{animals[1]}."
+puts "the 3rd animal is #{animals[2]}."
+puts "The first animal is #{animals[0]}."
+puts "The animal at 3 is #{animals[3]}."
+puts "The fifth animal is #{animals[4]}."
+puts "The animal at 2 is #{animals[2]}."
+puts "The sixth animal is #{animals[5]}."
+puts "The animal at 4 is #{animals[4]}."
+puts "All the animals are: #{animals[]}."

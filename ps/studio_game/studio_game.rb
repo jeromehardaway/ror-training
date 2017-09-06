5e295019633fb8ff6f90092c2d2028725574e2e2
Jerome Hardaway <jerome@vetswhocode.io>
2017-09-06 10:59:29 -0500

    save

--- ps/studio_game/studio_game.rb
+++ ps/studio_game/studio_game.rb
@@ -1,11 +1,15 @@
 class Player
+
+  attr_accessor :name
+  attr_reader :health
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
 
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def blam
@@ -17,13 +21,36 @@ class Player
     @health += 15
     puts "#{@name} got w00ted!"
   end
+
+  def score
+    @health + @name.length
+  end
+
 end
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-puts player1
-puts player2
-player3.w00t
-puts player3
+players = [player1, player2, player3]
+
+puts "There are #{players.size} players in the game:"
+players.each do |player|
+  puts player
+end
+
+players.each do |player|
+  puts player.health
+end
+
+players.each do |player|
+  player.blam
+  player.w00t
+  player.w00t
+  puts player
+end
+
+players.pop
+player4 = Player.new("Shemp", 90)
+players.push(player4)
+puts players
